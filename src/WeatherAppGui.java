30b68da721188157682ad9604918c07343d120fc
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-03-14 10:06:25 +0100

    feat: search text field & button

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -1,4 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatherAppGui extends JFrame {
   public WeatherAppGui() {
@@ -13,5 +18,35 @@ public class WeatherAppGui extends JFrame {
     setLayout(null);
 
     setResizable(false);
+
+    addGuiComponents();
+  }
+
+  private void addGuiComponents() {
+
+    JTextField searchTextField = new JTextField();
+
+    searchTextField.setBounds(15, 15, 351, 45);
+
+    searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+    add(searchTextField);
+
+    JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+    searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    searchButton.setBounds(375, 13, 47, 45);
+    add(searchButton);
+  }
+
+  private ImageIcon loadImage(String resourcePath) {
+    try {
+      BufferedImage image = ImageIO.read(new File(resourcePath));
+      return new ImageIcon(image);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Could not fin resource");
+    return null;
   }
 }
