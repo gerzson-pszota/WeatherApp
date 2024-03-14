a90c5ff5949dbc4b9c2c2df9b16e39b54969f1e9
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-03-14 10:24:46 +0100

    feat: weather image, temperature, humidity & windspeed

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -37,6 +37,40 @@ public class WeatherAppGui extends JFrame {
     searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
     searchButton.setBounds(375, 13, 47, 45);
     add(searchButton);
+
+    JLabel weatherConditionImage = new JLabel(loadImage("src/assets/cloudy.png"));
+    weatherConditionImage.setBounds(0, 125, 450, 217);
+    add(weatherConditionImage);
+
+    JLabel temperatureText = new JLabel("10 C");
+    temperatureText.setBounds(0, 350, 450, 54);
+    temperatureText.setFont(new Font("Dialog", Font.BOLD, 48));
+    temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+    add(temperatureText);
+
+    JLabel weatherConditionDesc = new JLabel("Cloudy");
+    weatherConditionDesc.setBounds(0, 405, 450, 36);
+    weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN, 32));
+    weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
+    add(weatherConditionDesc);
+
+    JLabel humidityImage = new JLabel(loadImage("src/assets/humidity.png"));
+    humidityImage.setBounds(15, 500, 74, 66);
+    add(humidityImage);
+
+    JLabel humidityText = new JLabel("<html><b>Humidity</b> 100%</html>");
+    humidityText.setBounds(90, 500, 85, 55);
+    humidityText.setFont(new Font("Dialog", Font.PLAIN, 16));
+    add(humidityText);
+
+    JLabel windspeedImage = new JLabel(loadImage("src/assets/windspeed.png"));
+    windspeedImage.setBounds(220, 500, 74, 66);
+    add(windspeedImage);
+
+    JLabel windspeedText = new JLabel("<html><b>Windspeed</b> 15km/h</html>");
+    windspeedText.setBounds(310, 500, 85, 55);
+    windspeedText.setFont(new Font("Dialog", Font.PLAIN, 16));
+    add(windspeedText);
   }
 
   private ImageIcon loadImage(String resourcePath) {
