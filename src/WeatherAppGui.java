2f5321024c969a8c2f8ca3dbfe7d29225389a96c
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-03-14 09:54:49 +0100

    feat: GUI

---
+++ src/WeatherAppGui.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class WeatherAppGui extends JFrame {
+  public WeatherAppGui() {
+    super("Weather App");
+
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    setSize(450, 650);
+
+    setLocationRelativeTo(null);
+
+    setLayout(null);
+
+    setResizable(false);
+  }
+}
